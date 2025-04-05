137cf796c3542ceeeac7428cf04f3e0e2e40400b
Pedro <pedro.saravia.q@uni.pe>
2025-04-05 09:13:36 -0500

    add empty service in transaction service

--- application/services/TransactionService.py
+++ application/services/TransactionService.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from domain.models import Transaction
 
@@ -11,8 +11,10 @@ class TransactionService:
         credit_total = 0.0
         debit_total = 0.0
         for t in self.transactions:
+            # * Add to credit total if it's a credit.
             if t.type.value == "Crédito":
                 credit_total += t.amount
+            # * Add to debit total if it's a debit.
             elif t.type.value == "Débito":
                 debit_total += t.amount
         return credit_total - debit_total
--- tests/application/test_transaction_service.py
+++ tests/application/test_transaction_service.py
@@ -5,21 +5,29 @@ from domain.models import Transaction
 
 
 @pytest.fixture
-def fake_transactions():
-    return [
-        Transaction(id="1", type="Crédito", amount=100.0),
-        Transaction(id="2", type="Débito", amount=50.0),
-        Transaction(id="3", type="Crédito", amount=200.0),
-        Transaction(id="4", type="Débito", amount=75.0),
-        Transaction(id="5", type="Crédito", amount=150.0),
-    ]
+def transaction_service():
+    return TransactionService(
+        [
+            Transaction(id="1", type="Crédito", amount=100.0),
+            Transaction(id="2", type="Débito", amount=50.0),
+            Transaction(id="3", type="Crédito", amount=200.0),
+            Transaction(id="4", type="Débito", amount=75.0),
+            Transaction(id="5", type="Crédito", amount=150.0),
+        ]
+    )
 
 
 @pytest.fixture
-def transaction_service(fake_transactions):
-    return TransactionService(fake_transactions)
+def empty_service():
+    return TransactionService([])
 
 
+# * --------------------- get_final_balance() ---------------------
 def test_final_balance(transaction_service):
     expected_balance = 325.0
     assert transaction_service.get_final_balance() == expected_balance
+
+
+def test_empty_final_balance(empty_service):
+    expected_balance = 0.0
+    assert empty_service.get_final_balance() == expected_balance
